426a4830c464e7655ca12a2d1df8be33878a513f
Agnieszka Matyja <agmatyja@gmail.com>
2019-06-08 00:18:23 +0200

    search works

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-ReactDOM.render(
-  (<App />),
-  document.getElementById('root')
-);
+class User extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+        <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+      </div>
+    );
+  }
+}
+class UsersList extends React.Component {
+  get users() {
+    return this.props.users.map(user => <User key={user.id} user={user}/>);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.users}
+      </div>
+    );
+  }
+}
 class App extends React.Component {
   constructor() {
     super();
@@ -40,26 +59,7 @@ class App extends React.Component {
     );
   }
 }
-class UsersList extends React.Component {
-  get users() {
-    return this.props.users.map(user => <User key={user.id} user={user}/>);
-  }
-
-  render() {
-    return (
-      <div>
-        {this.users}
-      </div>
-    );
-  }
-}
-class User extends React.Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
-        <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
-      </div>
-    );
-  }
-}
+ReactDOM.render(
+  (<App />),
+  document.getElementById('root')
+);
